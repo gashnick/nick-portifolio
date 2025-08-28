cc2c596b38c5c15c2d0bd1cff3fa8c7897b916d4
Gashumba Nicolas <gashnicolas@gmail.com>
2025-08-28 11:14:45 +0200

    added responsivenes

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ function PageTransition() {
       element.classList.add("active");
     }
   });
+
+  // Toggle theme button
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
 }
 
 PageTransition();
